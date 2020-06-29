a04e6c7873f1be5373eeb354bd0d0786896cf341
Luis Fernando García Castañeda <lfgc851@gmail.com>
2020-06-29 17:50:41 -0500

    Simplex noise
    Adding simplex noise to te Generate operation

--- Ravnos/Assets/Scripts/Map/MapGenerator.cs
+++ Ravnos/Assets/Scripts/Map/MapGenerator.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 using System;
 
 [System.Serializable]
-public struct TerrainType{
+public struct TerrainType
+{
     public float heigth;
     public string name;
     public Color colour;
 }
 
 
-public class MapGenerator : MonoBehaviour{
-    public enum DrawMode{NoiseMap, ColourMap};
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode { NoiseMap, ColourMap };
     public DrawMode drawMode;
 
     public int mapWidth;
@@ -21,38 +23,56 @@ public class MapGenerator : MonoBehaviour{
     public bool autoUpdate;
 
     public int octaves;
-    [Range(0,1)]
+    [Range(0, 1)]
     public float persistance;
     public float lacunarity;
-
-    //public int seed;
     public Vector2 offset;
-    public bool SimplexNoiseVar;
+
+    //tipos de ruido
+    public enum NoiseSelector { Perlin, Simplex, Value, Voronoi, Worley };
+    public NoiseSelector noiseSelector;
 
     public TerrainType[] regions;
 
-    public void GenerateMap(){
-        System.Random random = new System.Random(); 
-        Debug.Log("Generando mapa");
-        int RandomSeed = random.Next(0,100000);
+    public void GenerateMap()
+    {
+        System.Random random = new System.Random();
+        int RandomSeed = random.Next(0, 100000);
+
         System.Random local_seed = new System.Random(RandomSeed);
-        int local_seed_ = local_seed.Next(-100000,100000);
+        int int_seed = local_seed.Next(-100000, 100000);
         float[,] NoiseMap;
-        if(this.SimplexNoiseVar == true){
-            Debug.Log("Simplex activado");
-            NoiseMap = SimplexNoise.Calc2D(this.mapWidth, this.mapHeigth, this.noiseScale);
-        }else{
-            Debug.Log("Perlin activado");
-            NoiseMap = PerlinNoise.PerlinNoiseMapGenerator(this.mapWidth, this.mapHeigth, local_seed_, this.noiseScale, octaves, persistance,lacunarity, offset);
+
+        if (this.noiseSelector == NoiseSelector.Perlin)
+        {
+            NoiseMap = PerlinNoise.PerlinNoiseMapGenerator(this.mapWidth, this.mapHeigth, int_seed, this.noiseScale, octaves, persistance, lacunarity, offset);
+        }
+        else if (this.noiseSelector == NoiseSelector.Simplex)
+        {
+            NoiseMap = SimplexNoise.SimplexNoiseMapGenerator(this.mapWidth,this.mapHeigth, int_seed, octaves, offset);
+        }/*else if(this.noiseSelector == NoiseSelector.Value){
+
+        }else if(this.noiseSelector == NoiseSelector.Voronoi){
+
+        }else if(this.noiseSelector == NoiseSelector.Worley){
+
+        }*/
+        else
+        {
+            //default: perlin noise
+            NoiseMap = PerlinNoise.PerlinNoiseMapGenerator(this.mapWidth, this.mapHeigth, int_seed, this.noiseScale, octaves, persistance, lacunarity, offset);
         }
 
         Color[] colourMap = new Color[mapWidth * mapHeigth];
-        Debug.Log("Generando mapa de colores");
-        for(int y = 0; y < mapHeigth; y++){
-            for(int x = 0; x < mapWidth; x++){
-                float currentHeigth = NoiseMap [x, y];
-                for(int i = 0; i < regions.Length; i++){
-                    if(currentHeigth <= regions[i].heigth){
+        for (int y = 0; y < mapHeigth; y++)
+        {
+            for (int x = 0; x < mapWidth; x++)
+            {
+                float currentHeigth = NoiseMap[x, y];
+                for (int i = 0; i < regions.Length; i++)
+                {
+                    if (currentHeigth <= regions[i].heigth)
+                    {
                         colourMap[y * mapWidth + x] = regions[i].colour;
                         break;
                     }
@@ -61,26 +81,32 @@ public class MapGenerator : MonoBehaviour{
         }
 
         MapDisplay display = FindObjectOfType<MapDisplay>();
-        if(drawMode == DrawMode.NoiseMap){
-            Debug.Log("Creando mapa de ruido");
+        if (drawMode == DrawMode.NoiseMap)
+        {
             display.DrawTexture(TextureGenerator.TextureFromHeightMap(NoiseMap));
-        }else if(drawMode == DrawMode.ColourMap){
-            Debug.Log("Creando mapa de colores");
+        }
+        else if (drawMode == DrawMode.ColourMap)
+        {
             display.DrawTexture(TextureGenerator.TextureFromColourMap(colourMap, mapWidth, mapHeigth));
         }
     }
 
-    void OnValidate(){
-        if(mapWidth < 1){
+    void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
             mapWidth = 1;
         }
-        if(mapHeigth < 1){
+        if (mapHeigth < 1)
+        {
             mapHeigth = 1;
         }
-        if(lacunarity < 1){
+        if (lacunarity < 1)
+        {
             lacunarity = 1;
         }
-        if(octaves < 0){
+        if (octaves < 0)
+        {
             octaves = 0;
         }
     }
